cb91d8a231f3df488d54d989dd488637a8037060
Justin <justin@mysticrealms.net>
2015-03-17 02:03:16 -0400

    main menu stuff

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject CurrentMenu;
+
+	public void Start() {
+		ShowMenu(CurrentMenu);
+	}
+
+	public void ShowMenu(GameObject menu){
+		if(CurrentMenu != null)
+			CurrentMenu.GetComponent<Menu>().IsOpen = false;
+		
+		CurrentMenu = menu;
+		CurrentMenu.GetComponent<Menu>().IsOpen = true;
+	}﻿
+
+	public void Quit() {
+		Application.Quit();
+	}
+	
+}
