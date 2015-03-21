f79e69f1dc492352c1b80f0942dfe02c2775e2a6
Matosvictor22 <matosvictor22@gmail.com>
2015-03-21 11:27:13 -0400

    Pushed Tab-able Face HUD
    We can delete the other fork now, I started working directly on main

---
+++ Assets/FaceHUD.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(RawImage))]
+
+public class FaceHUD : MonoBehaviour {
+	public RawImage faceHUD;
+	// Use this for initialization
+	void Start () {
+		faceHUD = GetComponent<RawImage>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//If the User presses tab, it toggles the image.
+		if (Input.GetKeyDown (KeyCode.Tab)){
+			if(faceHUD.enabled == true){
+				Debug.Log(faceHUD.IsActive());
+				faceHUD.enabled = false;
+			}
+			else{
+				faceHUD.enabled = true;	
+			}
+		}
+	}
+
+
+}
