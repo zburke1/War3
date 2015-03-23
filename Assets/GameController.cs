8e5c49330b70b2f7d92e51c24ebcd0f2367498c2
zburke1 <zburke1@ufl.edu>
2015-03-23 15:38:51 -0400

    Hard code array for faces. Most likely need to overhaul, but works for now.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,9 +6,12 @@ public class GameController : MonoBehaviour {
 	public GameObject Cube;
 	private GameObject clone;
 	private int count = 0;
-	
+	private int blockNum=1;
+	public Object[,] board;
+	private Object temp;
 	// Use this for initialization
 	void Start () {
+		board = new Object[7,10];
 		//Builds bottom,forwards,up
 		for (float y = 0; y < 3; y++) {
 			count = 0;
@@ -16,16 +19,56 @@ public class GameController : MonoBehaviour {
 					  for(float z=0;z<3;z++){
 						  
 						 var obj = Instantiate(Cube, new Vector3(x*1.2f, y*1.2f, z*1.2f), Quaternion.identity);
-						 obj.name = "TEST";
-						//clone.position(x*1.2f, y*1.2f, z*1.2f);
+						 obj.name = "Block"+blockNum;
+						 blockNum++;
 					  }
 					  count++;
 				  }
 			 }
+			 instantiateBoard();
+			 deleteTest();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
 	
+	private void instantiateBoard(){
+		blockNum=-2;
+		int [] faceA = {19, 22, 25, 10, 13, 16,1, 4, 7};
+		int [] faceB = {21, 20, 19,12,11,10,3,2,1};
+		int [] faceC = {27,24,21,18,15,12,9,6,3};
+		int [] faceD = {25,26,27,16,17,18,7,8,9};
+		int [] faceE = {21,24,27,20,23,26,19,22,25};
+		int [] faceF = {1,4,7,2,5,8,3,6,9};
+		
+		
+		for(int i=1;i<10;i++)
+		board[1,i]=GameObject.Find("Block"+faceA[i-1]);
+		
+		for(int i=1;i<10;i++)
+		board[2,i]=GameObject.Find("Block"+faceB[i-1]);
+		
+		for(int i=1;i<10;i++)
+		board[3,i]=GameObject.Find("Block"+faceC[i-1]);
+		
+		for(int i=1;i<10;i++)
+		board[4,i]=GameObject.Find("Block"+faceD[i-1]);
+		
+		for(int i=1;i<10;i++)
+		board[5,i]=GameObject.Find("Block"+faceE[i-1]);
+		
+		for(int i=1;i<10;i++)
+		board[6,i]=GameObject.Find("Block"+faceF[i-1]);
+		
+		
+		
+	}
+	private void deleteTest(){
+		for(int i=1;i<10;i++){
+			 //Destroy(board[5,i]);
+		}
+
 	}
 }
