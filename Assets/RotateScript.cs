e4242781762ed8061287636fa0a2d5d1e9c53d2a
zburke1 <zburke1@ufl.edu>
2015-03-24 14:24:26 -0400

    Added get and set functions to gameController for access from rotate script

--- Assets/RotateScript.cs
+++ Assets/RotateScript.cs
@@ -1,50 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RotateScript : MonoBehaviour {
-	public Rect buttonRect = new Rect(Screen.width/4, 50, 50, 30);
-	public GameObject test;
-	bool RotateLeft = false;
-	float startVal = 0f; 
-	private Vector3 Rotation = new Vector3 (0,0,1);
-	//Rotation.Transform.eulerAngles = test.Transform.eulerAngles;
-	// Use this for initialization
-	void Start () {
+using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour{
+	public GameController m_gameController;
+	private GameObject[,] rotateBoard = new GameObject[7,10];
+	private GameObject[,] rotateFace = new GameObject[7,10];
+	private bool init;
+	
+	void Start(){
+		m_gameController = GameObject.FindObjectOfType(typeof(GameController)) as GameController;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		//transform.Rotate(Vector3.forward, Time.deltaTime*15,Space.Self);
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			
-			//RotateLeft = true; 
-			
+	void Update(){
+		//This code is checking to see if the board has been initialized in the GameController script
+		if(m_gameController.getSingleFace(1,1)!=null&& !init){
+			Debug.Log("Initialize Logic.......");
+			initializeRotate();
+			init = true;
 		}
-		//if (test.transform.eulerAngles.z < startVal + 90 && RLeftCheck) {
-		if (RotateLeft) {
-			startVal = test.transform.eulerAngles.z;
-				GameObject.Find("Slice1").transform.RotateAround (test.transform.position, Vector3.forward, 70 * Time.deltaTime);
-				print (test.transform.eulerAngles);
-				} 
-				//		else if (!RotateLeft) {
-				//					transform.RotateAround (test.transform.position, Vector3.back, 70 * Time.deltaTime);
-				//				}
-				//		else {	
-				//						RotateLeft = false;
-				//				}
-				
-				}
-				
-				
-				void OnGUI(){
-				if (GUI.Button (buttonRect, "PLEASE!!!!!")) {
-					RotateLeft = true;
-				}
-				Event e = Event.current;
-				if (e.isKey)
-					Debug.Log ("Detected key code: " + e.keyCode);
-				
+		
+		if(Input.GetKey(KeyCode.Space)){
+			//Using for testing
+			m_gameController.pushBoard(rotateFace);
 			}
-			
+		}
+	
+		private void initializeRotate(){
+			rotateBoard = m_gameController.getBoard();
+			Debug.Log("Rotate Board Logic Created");
+			rotateFace = m_gameController.getFaceArray();
+			Debug.Log("Rotate Face Logic Created");
 			}
+		}
 			
