86c0d56b71047c3954a94ad36c105c5617b3a5c4
Matosvictor22 <matosvictor22@gmail.com>
2015-04-08 19:43:18 -0400

    Values of Cube mean something now!

--- Assets/TileNumDisplay.cs
+++ Assets/TileNumDisplay.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
+[RequireComponent (typeof(Text))]
+
 public class TileNumDisplay : MonoBehaviour {
+	GameObject gameControllerObject;
+	GameController gc;
+	public int face;
+	public int tile;
+	private Text troopNumText;
+	public int troopNum =0;
 
 	// Use this for initialization
 	void Start () {
+		gc = GameObject.FindObjectOfType (typeof(GameController)) as GameController;
+		troopNumText = GetComponent<Text> ();
+		troopNum = gc.getTroops (face, tile);
 	
+	
+
+		//gameControllerObject.Find("GameController");
+		//gc = (gc) gameControllerObject.GetComponent(typeof(GameController));
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		troopNumText.text = gc.getTroops (face, tile).ToString ();
+		//debug.log( gc.randomInt);
 	}
+
 }
