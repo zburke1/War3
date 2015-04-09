301c842b4abed51053197fbb4243e8c53f9b1489
Matosvictor22 <matosvictor22@gmail.com>
2015-04-09 01:29:56 -0400

    Finalized Implementation for FaceHUD

---
+++ Assets/tabToggle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Text))]
+public class tabToggle : MonoBehaviour {
+	public Text text;
+	// Use this for initialization
+	void Start () {
+	text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Tab)){
+			if(text.enabled == true){
+				Debug.Log(text.IsActive());
+				text.enabled = false;
+			}
+			else{
+				text.enabled = true;	
+			}
+			
+	}
+}
+}
