99ad5dd33d729c82c10801021e46fd005055f1d8
Matosvictor22 <matosvictor22@gmail.com>
2015-04-12 12:54:16 -0400

    Added Phase UI and Added Phase Functionality ENUM

---
+++ Assets/PhaseButton.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+[RequireComponent (typeof(Toggle))]
+
+public class PhaseButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler {
+	PhaseHandler ph;
+	public string hoverText;
+	Toggle theToggle; 
+	public enum  PhaseToggle {spawn, rotate, battle, end};
+	public PhaseToggle phaseType;
+	// Use this for initialization
+
+	void Start () {
+		ph = GameObject.FindObjectOfType(typeof(PhaseHandler)) as PhaseHandler;
+	//	ph = GameObject.Find("PhaseHandler").GetComponent<PhaseHandler>();
+		theToggle = GetComponent<Toggle> ();
+
+	}
+	
+
+
+	void Update () {
+	//theToggle.onC
+	}
+
+	public void OnPointerClick(PointerEventData data)
+	{
+		switch(phaseType){
+			
+			
+		case PhaseToggle.rotate:
+			ph.startRotationPhase();
+			Debug.Log ("Starting Rotation Phase");
+			break;
+			
+		case PhaseToggle.battle:
+			ph.startBattlePhase();
+			Debug.Log ("Starting Battle Phase");
+			break;
+			
+		case PhaseToggle.end:
+			ph.disableAllToggles();
+			Debug.Log ("Turn End");
+			break;
+			
+		default: 
+			break;
+			
+			//invoke start new turn with]
+			//ph.startNewTurn ();
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData data){
+	
+	}
+
+	public void OnPointerExit(PointerEventData data){
+	}
+
+}
---
+++ Assets/PhaseHandler.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PhaseHandler : MonoBehaviour {
+	//These variables contain the toggle objects which we will use for phase visual and interaction
+	Toggle spawnToggle;
+	Toggle rotateToggle;
+	Toggle battleToggle;
+	Toggle endToggle;
+	enum Phase {spawnPhase, rotatePhase, battlePhase, endPhase};
+	Phase currentPhase;
+
+	// Use this for initialization
+	void Start () {
+		rotateToggle = GameObject.Find("RotatePhaseToggle").GetComponent<Toggle>();
+		spawnToggle = GameObject.Find("SpawnPhaseToggle").GetComponent<Toggle>();
+		battleToggle = GameObject.Find("BattlePhaseToggle").GetComponent<Toggle>();
+		endToggle = GameObject.Find("EndPhaseToggle").GetComponent<Toggle>();
+		currentPhase = Phase.spawnPhase;
+
+
+		spawnToggle.isOn = true;
+		rotateToggle.isOn = false;
+		battleToggle.isOn = false;
+		endToggle.isOn = false;
+
+		spawnToggle.interactable = true;
+		rotateToggle.interactable = true;
+		battleToggle.interactable = false;
+		endToggle.interactable = false;
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(currentPhase == Phase.spawnPhase){
+		
+		}
+
+		else if(currentPhase == Phase.rotatePhase){
+		}
+
+		else if(currentPhase == Phase.battlePhase){
+		}
+
+		else if(currentPhase == Phase.endPhase){
+
+		}
+
+	}
+
+	public virtual void disableAllToggles(){
+		spawnToggle.isOn = false;
+		rotateToggle.isOn = false;
+		battleToggle.isOn = false;
+		endToggle.isOn = false;
+
+		spawnToggle.interactable = false;
+		rotateToggle.interactable = false;
+		battleToggle.interactable = false;
+		endToggle.interactable = false;
+	}
+
+	public virtual void startNewTurn(){
+		spawnToggle.isOn = true;
+		rotateToggle.interactable = true;
+		rotateToggle.isOn = false;
+		battleToggle.isOn = false;
+
+	}
+
+	public virtual void startRotationPhase(){
+		battleToggle.interactable = true;
+		spawnToggle.isOn = false;
+		spawnToggle.interactable = false;
+		rotateToggle.isOn = true;
+		battleToggle.isOn = false;
+
+	}
+
+	public virtual void startBattlePhase(){
+		rotateToggle.isOn = false;
+		rotateToggle.interactable = false;
+		battleToggle.isOn = true;
+		endToggle.interactable = true;
+	}
+
+}
+
