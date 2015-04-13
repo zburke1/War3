9f130229a175c080ee6644633f6ceb7e58365ddf
Matosvictor22 <matosvictor22@gmail.com>
2015-04-12 21:39:59 -0400

    Added Text for Player Turn

---
+++ Assets/PlayerTurntext.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class PlayerTurntext : MonoBehaviour {
+
+
+	string playerString;
+	Text whoseTurn;
+	GameController go;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		whoseTurn = GetComponent<Text>();
+		go = GameObject.FindObjectOfType(typeof(GameController)) as GameController;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerString = "Turn: Player " + go.currentPlayer.ToString ();
+		whoseTurn.text = playerString;
+	}
+}
+
